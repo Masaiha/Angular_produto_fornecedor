17c3e79581f4cba7f552c04cd8db97e03ec4eaa4
Masaiha <masanori.iha@gmail.com>
2020-10-22 09:53:10 -0300

    instalando ng bootstrap

--- front-end/loja/src/app/app.module.ts
+++ front-end/loja/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { NavegacaoModule } from './navegacao/navegacao.module';
 import { AppRoutingModule } from './app.routes';
@@ -13,6 +15,7 @@ import { AppRoutingModule } from './app.routes';
     BrowserModule,
     AppRoutingModule,
     NavegacaoModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
