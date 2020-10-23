0c939dba6dabd4f6684bacaf46acad0a36d5ccd4
Masaiha <masanori.iha@gmail.com>
2020-10-23 11:57:21 -0300

    implementando servico de fornecedor

---
+++ front-end/loja/src/app/fornecedor/services/fornecedor.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+import { Fornecedor } from '../models/fornecedor';
+import { BaseService } from 'src/app/services/base.service';
+
+@Injectable()
+export class FornecedorService extends BaseService {
+
+    fornecedor: Fornecedor = new Fornecedor();
+
+    constructor(private http: HttpClient) { super() 
+    
+        this.fornecedor.nome = "Teste Fake"
+        this.fornecedor.documento = "32165498754"
+        this.fornecedor.ativo = true
+        this.fornecedor.tipoFornecedor = 1
+    }
+
+    obterTodos(): Observable<Fornecedor[]> {
+        return this.http
+            .get<Fornecedor[]>(this.UrlServiceV1 + "fornecedores")
+            .pipe(catchError(super.ErrorService));
+    }
+
+    obterPorId(id: string): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }
+
+    novoFornecedor(fornecedor: Fornecedor): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }
+
+    atualizarFornecedor(fornecedor: Fornecedor): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }
+
+    excluirFornecedor(id: string): Observable<Fornecedor> {
+        return new Observable<Fornecedor>();
+    }    
+}
