9de8506e0bf89d33162ed798569cb85931046700
Masaiha <masanori.iha@gmail.com>
2020-10-23 09:48:42 -0300

    instalando e configurando o toastr com ngx-toastr

--- front-end/loja/src/app/app.module.ts
+++ front-end/loja/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppComponent } from './app.component';
 import { NavegacaoModule } from './navegacao/navegacao.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     NavegacaoModule,
     NgbModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
